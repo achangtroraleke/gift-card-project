eea094ebd622f4671e911e1562f717f49a9d2f48
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2024-01-22 18:18:28 -0500

    hopefully fixed settings.

--- gift_cards/settings.py
+++ gift_cards/settings.py
@@ -51,6 +51,7 @@ AUTH_USER_MODEL = 'base.MyCustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- gift_cards/urls.py
+++ gift_cards/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
